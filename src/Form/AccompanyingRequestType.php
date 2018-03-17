0f97b538a28807a380adca364b30d5026a87e96f
Quentin Raimbault <quentin.raimbault@hetic.net>
2018-03-17 18:05:59 +0100

    💄 Add dropdown list for actual school level and wanted promotion

--- src/Form/AccompanyingRequestType.php
+++ src/Form/AccompanyingRequestType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\AccompanyingRequest;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -17,8 +18,31 @@ class AccompanyingRequestType extends AbstractType
             ->add('firstName', TextType::class, ['label' => 'Prénom'])
             ->add('lastName', TextType::class, ['label' => 'Nom de famille'])
             ->add('email', EmailType::class, ['label' => 'Addresse e-mail'])
-            ->add('wantedPromotion', TextType::class, ['label' => 'Promotion demandée'])
-            ->add('actualSchoolLevel', TextType::class, ['label' => 'Niveau d\'études actuel'])
+            ->add('wantedPromotion', ChoiceType::class, [
+                'choices' => [
+                    'WEB1' => 'WEB1',
+                    'WEB2' => 'WEB2',
+                    'WEB3' => 'WEB3',
+                    'WM1' => 'WM1',
+                    'WM2' => 'WM2',
+                    'WM3' => 'WM3',
+                    '3D1' => '3D1',
+                    '3D2' => '3D2',
+                    '3D3' => '3D3',
+                ],
+                'label' => 'Promotion demandée',
+            ])
+            ->add('actualSchoolLevel', ChoiceType::class, [
+                'choices' => [
+                    'BAC' => 'BAC',
+                    'BAC + 1' => 'BAC + 1',
+                    'BAC + 2' => 'BAC + 2',
+                    'BAC + 3' => 'BAC + 3',
+                    'BAC + 4' => 'BAC + 4',
+                    'BAC + 5' => 'BAC + 5',
+                ],
+                'label' => 'Niveau d\'études actuel',
+            ])
             ->add('twitter', TextType::class, ['label' => 'Twitter'])
             ->add('facebook', TextType::class, ['label' => 'Facebook'])
             ->add('phoneNumber', TextType::class, ['label' => 'Téléphone (+33)'])
