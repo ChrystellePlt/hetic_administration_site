826d1658851216c8cc6a9b48829cf981ff2c9e50
Quentin Raimbault <quentin.raimbault@hetic.net>
2018-03-17 17:01:10 +0100

    💄 Add link to request listing on dashboard
     #7

--- src/Migrations/Version20180317154032.php
+++ src/Migrations/Version20180317154032.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types = 1);
+<?php
+
+declare(strict_types=1);
 
 namespace DoctrineMigrations;
 
@@ -13,7 +15,7 @@ class Version20180317154032 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE app_accompanying_requests (id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', first_name VARCHAR(70) NOT NULL, last_name VARCHAR(70) NOT NULL, email VARCHAR(255) NOT NULL, wanted_promotion VARCHAR(20) NOT NULL, actual_school_level VARCHAR(20) NOT NULL, twitter VARCHAR(30) NOT NULL, facebook VARCHAR(140) NOT NULL, phone_number VARCHAR(15) NOT NULL, wanted_speciality VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_F499F243E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
     }
@@ -21,7 +23,7 @@ class Version20180317154032 extends AbstractMigration
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE app_accompanying_requests');
     }
